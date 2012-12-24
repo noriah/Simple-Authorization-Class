81e1108e5a47338a0bdca992632976566a5f7bd3
noriah <vix@noriah.dev>
2012-12-24 13:35:01 -0800

    Create auth_strings.php

---
+++ auth_strings.php
@@ -0,0 +1,9 @@
+<?php
+
+$flagS['basic'] = "You must have the <b>basic</b> permission flag to continue.";
+$flagS['upload'] = "You must have the <b>upload</b> permission flag to use the Upload Tool/View User Folder.";
+$flagS['browse'] = "You must have the <b>browse</b> permission flag to browse folders.";
+$flagS['system'] = "You must have the <b>system</b> permission flag to continue.";
+$flagS['root'] = "You must have the <b>root</b> permission flag to continue.";
+
+?>
