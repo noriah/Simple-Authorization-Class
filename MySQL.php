7163b51941d700299ae68fe75b5709501755cc67
noriah <vix@noriah.dev>
2012-12-24 13:36:36 -0800

    Create MySQL.php

---
+++ MySQL.php
@@ -0,0 +1,9 @@
+<?
+if($_SERVER['PHP_SELF']=="/common/php/MySQL.php"){echo "Stop Snooping!"; die;}
+$dbInfo['user'] = 'USERNAME_HERE';
+$dbInfo['pass'] = 'PASSWORD_HERE';
+$dbInfo['server'] = 'SERVER_HERE';
+
+$dbs['main'] = 'MAIN_DATABASE_HERE';
+
+?>
